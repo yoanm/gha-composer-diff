c315ee7a325f300d0adf038528db7e27ed50bae8
Yoanm <4410697+yoanm@users.noreply.github.com>
2026-05-07 18:13:00 +0200

    Add missing files

---
+++ DOC.md
@@ -0,0 +1,36 @@
+# ghadepsdiff
+
+## Functions
+
+### func [Run](/main.go#L12)
+
+`func Run(cfg *Config) (string, error)`
+
+## Types
+
+### type [Config](/types.go#L12)
+
+`type Config struct { ... }`
+
+### type [ManagerType](/types.go#L18)
+
+`type ManagerType string`
+
+#### Constants
+
+```golang
+const (
+    ComposerManager ManagerType = "Composer"
+)
+```
+
+### type [PkgManagerInput](/types.go#L3)
+
+`type PkgManagerInput struct { ... }`
+
+## Sub Packages
+
+* [cmd/gha-deps-diff](./cmd/gha-deps-diff)
+
+---
+Readme created from Go doc with [goreadme](https://github.com/posener/goreadme)
---
+++ cmd/gha-deps-diff/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+
+	"ghadepsdiff"
+)
+
+var (
+	previousReqFileFlag  string
+	previousLockFileFlag string
+	currentReqFileFlag   string
+	currentLockFileFlag  string
+	debugModeFlag        bool
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var err error
+
+	parseFlags()
+
+	if debugModeFlag {
+		slog.SetLogLoggerLevel(slog.LevelDebug)
+	}
+
+	var cfg *ghadepsdiff.Config
+	if cfg, err = parseInputs(); err != nil {
+		return err
+	}
+
+	var chgSummary string
+	if chgSummary, err = ghadepsdiff.Run(cfg); err != nil {
+		return err
+	}
+
+	fmt.Println(chgSummary)
+
+	return nil
+}
+
+func parseFlags() {
+	flag.StringVar(&previousReqFileFlag, "previous-req", "", "Previous requirement file path")
+	flag.StringVar(&previousLockFileFlag, "previous-lock", "", "Previous lock file path")
+	flag.StringVar(&currentReqFileFlag, "current-req", "", "Current requirement file path")
+	flag.StringVar(&currentLockFileFlag, "current-lock", "", "Current lock file path")
+	flag.BoolVar(&debugModeFlag, "d", false, "Enable debug logs")
+
+	flag.Parse()
+}
+
+func parseInputs() (*ghadepsdiff.Config, error) {
+	switch {
+	case "" == previousReqFileFlag:
+		return nil, errors.New("previous requirement file is missing")
+	case "" == previousLockFileFlag:
+		return nil, errors.New("previous lock file is missing")
+	case "" == currentReqFileFlag:
+		return nil, errors.New("current requirement file is missing")
+	case "" == currentLockFileFlag:
+		return nil, errors.New("current lock file is missing")
+	}
+
+	return &ghadepsdiff.Config{
+		Manager: ghadepsdiff.ComposerManager,
+		Previous: ghadepsdiff.PkgManagerInput{
+			Lock:        previousLockFileFlag,
+			Requirement: previousReqFileFlag,
+		},
+		Current: ghadepsdiff.PkgManagerInput{
+			Lock:        currentLockFileFlag,
+			Requirement: currentReqFileFlag,
+		},
+	}, nil
+}
